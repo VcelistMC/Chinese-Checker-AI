e1c85405f56c6cc0c670cc7b92b90f6c90948684
Vcelist <petoilluis@gmail.com>
2022-06-01 19:37:34 +0200

    added init board loop

--- game_model.py
+++ game_model.py
@@ -1,146 +1,50 @@
 class Game:
     def initBoard(self):
-        self.newBoard = [[" " for _ in range(self.cols)] for _ in range(self.rows)]
-        self.newBoard[0][12] = "G"
-        self.newBoard[1][11] = "R"
-        self.newBoard[1][13] = "G"
-        self.newBoard[2][10] = "G"
-        self.newBoard[2][12] = "G"
-        self.newBoard[2][14] = "G"
-        self.newBoard[3][9] = "G"
-        self.newBoard[3][11] = "G"
-        self.newBoard[3][13] = "G"
-        self.newBoard[3][15] = "G"
+        self.board = [[" " for _ in range(self.cols)] for _ in range(self.rows)]
+        
+        #init top start
+        start_ind = 12
+        end_ind = 12
 
-        self.newBoard[4][0] = "B"
-        self.newBoard[4][2] = "B"
-        self.newBoard[4][4] = "B"
-        self.newBoard[4][6] = "B"
-        self.newBoard[5][1] = "B"
-        self.newBoard[5][3] = "B"
-        self.newBoard[5][5] = "B"
-        self.newBoard[6][2] = "B"
-        self.newBoard[6][4] = "B"
-        self.newBoard[7][3] = "B"
+        for row in range(0, 4):
+            for col in range(start_ind, end_ind+1, 2):
+                self.board[row][col] = self.players[0]
+            start_ind -= 1
+            end_ind += 1
 
-        self.newBoard[9][3] = "P"
-        self.newBoard[10][2] = "P"
-        self.newBoard[10][4] = "P"
-        self.newBoard[11][1] = "P"
-        self.newBoard[11][3] = "P"
-        self.newBoard[11][5] = "P"
-        self.newBoard[12][0] = "P"
-        self.newBoard[12][2] = "P"
-        self.newBoard[12][4] = "P"
-        self.newBoard[12][6] = "P"
+        #init first section with dots
+        start_ind = 0
+        end_ind = 24
 
-        self.newBoard[9][21] = "O"
-        self.newBoard[10][20] = "O"
-        self.newBoard[10][22] = "O"
-        self.newBoard[11][19] = "O"
-        self.newBoard[11][21] = "O"
-        self.newBoard[11][23] = "O"
-        self.newBoard[12][18] = "O"
-        self.newBoard[12][20] = "O"
-        self.newBoard[12][22] = "O"
-        self.newBoard[12][24] = "O"
+        for row in range(4, 9):
+            for col in range(start_ind, end_ind+1, 2):
+                self.board[row][col] = "."
 
-        self.newBoard[4][18] = "Y"
-        self.newBoard[4][20] = "Y"
-        self.newBoard[4][22] = "Y"
-        self.newBoard[4][24] = "Y"
-        self.newBoard[5][19] = "Y"
-        self.newBoard[5][21] = "Y"
-        self.newBoard[5][23] = "Y"
-        self.newBoard[6][20] = "Y"
-        self.newBoard[6][22] = "Y"
-        self.newBoard[7][21] = "Y"
+            start_ind += 1
+            end_ind -= 1
+        
+        #init second empty section with dots
+        start_ind = 3
+        end_ind = 21
 
-        self.newBoard[16][12] = "R"
-        self.newBoard[15][11] = "R"
-        self.newBoard[15][13] = "R"
-        self.newBoard[14][10] = "R"
-        self.newBoard[14][12] = "R"
-        self.newBoard[14][14] = "R"
-        self.newBoard[13][9] = "R"
-        self.newBoard[13][11] = "R"
-        self.newBoard[13][13] = "R"
-        self.newBoard[13][15] = "R"
-
-        self.newBoard[4][8] = "."
-        self.newBoard[4][10] = "."
-        self.newBoard[4][12] = "."
-        self.newBoard[4][14] = "."
-        self.newBoard[4][16] = "."
-
-        self.newBoard[5][7] = "."
-        self.newBoard[5][9] = "."
-        self.newBoard[5][11] = "."
-        self.newBoard[5][13] = "."
-        self.newBoard[5][15] = "."
-        self.newBoard[5][17] = "."
-
-        self.newBoard[6][6] = "."
-        self.newBoard[6][8] = "."
-        self.newBoard[6][10] = "."
-        self.newBoard[6][12] = "."
-        self.newBoard[6][14] = "."
-        self.newBoard[6][16] = "."
-        self.newBoard[6][18] = "."
-
-        self.newBoard[7][5] = "."
-        self.newBoard[7][7] = "."
-        self.newBoard[7][9] = "."
-        self.newBoard[7][11] = "."
-        self.newBoard[7][13] = "."
-        self.newBoard[7][15] = "."
-        self.newBoard[7][17] = "."
-        self.newBoard[7][19] = "."
-
-        self.newBoard[8][4] = "."
-        self.newBoard[8][6] = "."
-        self.newBoard[8][8] = "."
-        self.newBoard[8][10] = "."
-        self.newBoard[8][12] = "."
-        self.newBoard[8][14] = "."
-        self.newBoard[8][16] = "."
-        self.newBoard[8][18] = "."
-        self.newBoard[8][20] = "."
-
-        self.newBoard[9][5] = "."
-        self.newBoard[9][7] = "."
-        self.newBoard[9][9] = "."
-        self.newBoard[9][11] = "."
-        self.newBoard[9][13] = "."
-        self.newBoard[9][15] = "."
-        self.newBoard[9][17] = "."
-        self.newBoard[9][19] = "."
-
-        self.newBoard[10][6] = "."
-        self.newBoard[10][8] = "."
-        self.newBoard[10][10] = "."
-        self.newBoard[10][12] = "."
-        self.newBoard[10][14] = "."
-        self.newBoard[10][16] = "."
-        self.newBoard[10][18] = "."
-
-        self.newBoard[11][7] = "."
-        self.newBoard[11][9] = "."
-        self.newBoard[11][11] = "."
-        self.newBoard[11][13] = "."
-        self.newBoard[11][15] = "."
-        self.newBoard[11][17] = "."
-
-        self.newBoard[12][8] = "."
-        self.newBoard[12][10] = "."
-        self.newBoard[12][12] = "."
-        self.newBoard[12][14] = "."
-        self.newBoard[12][16] = "."
+        for row in range(9, 13):
+            for col in range(start_ind, end_ind+1, 2):
+                self.board[row][col] = "."
+            start_ind -= 1
+            end_ind += 1
+        
+        #init bottom star
+        start_ind = 9
+        end_ind = 15
+        for row in range(13, 17):
+            for col in range(start_ind, end_ind+1, 2):
+                self.board[row][col] = self.players[1]
+            start_ind += 1
+            end_ind -= 1
 
     def __init__(self):
         self.cols = 25
-        self.players = ["G", "R"]
-        self.this = self
+        self.players = ["R", "B"]
         self.rows = 17
         self.rowsLengths = [1, 2, 3, 4, 13, 12, 11, 10, 9, 10, 11, 12, 13, 4, 3, 2, 1]
 
@@ -157,13 +61,13 @@ class Game:
     def printBoard(self):
         for i in range(self.rows):
             for j in range(self.cols):
-                print(self.newBoard[i][j], end=" ")
+                print(self.board[i][j], end=" ")
             print("\n")
 
     # executes a move, Note: This function assumes that the given next move is valid
     def move(self, pieceRow, pieceCol, destRow, destCol):
-        self.newBoard[destRow][destCol] = self.newBoard[pieceRow][pieceCol]
-        self.newBoard[pieceRow][pieceCol] = "."
+        self.board[destRow][destCol] = self.board[pieceRow][pieceCol]
+        self.board[pieceRow][pieceCol] = "."
 
     # get all moves from starting pos
     def getMoves(self, pieceRow, pieceCol):
@@ -172,7 +76,7 @@ class Game:
         for dir, cords in self.dirs.items():
             newRow = pieceRow + cords[0]
             newCol = pieceCol + cords[1]
-            if newRow < 0 or newCol < 0 or newRow >= self.rows or newCol >= self.cols or self.newBoard[newRow][
+            if newRow < 0 or newCol < 0 or newRow >= self.rows or newCol >= self.cols or self.board[newRow][
                 newCol] == " ":
                 continue
             moves[dir] = [newRow, newCol]
@@ -184,7 +88,7 @@ class Game:
         for dir, move in allMoves.items():
             row = move[0]
             col = move[1]
-            if self.newBoard[row][col] == ".":
+            if self.board[row][col] == ".":
                 validMoves.append(move)
                 continue
 
@@ -192,7 +96,7 @@ class Game:
             newRow = row + dirMove[0]
             newCol = col + dirMove[1]
 
-            if self.newBoard[newRow][newCol] == ".":
+            if self.board[newRow][newCol] == ".":
                 validMoves.append([newRow, newCol])
 
         return validMoves
@@ -205,7 +109,7 @@ class Game:
             end_index = 12
             for row in range(4):
                 for col in range(start_index, end_index+1, 2):
-                    if self.newBoard[row][col] == "R":
+                    if self.board[row][col] == "R":
                         player1Count += 1
                 start_index -= 1
                 end_index += 1
@@ -215,7 +119,7 @@ class Game:
             end_index = 12
             for row in range(13, 17):
                 for col in range(start_index, end_index + 1, 2):
-                    if self.newBoard[row][col] == "G":
+                    if self.board[row][col] == "G":
                         player1Count += 1
 
                 start_index -= 1
@@ -225,10 +129,10 @@ class Game:
 
 
 
-# game = Game()
+game = Game()
 
 # # print(moves)
-# game.printBoard()
+game.printBoard()
 # # print(game.getAllValidMoves(2, 12))
 # # game.printBoard()
 # print(game.is_win("R"))
